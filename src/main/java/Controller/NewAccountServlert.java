c1c4094a51ef430c9bf8ead3788533d20a2dbf8a
Pranay Goel <goel.2002pranay@gmail.com>
2023-09-26 02:24:21 +0530

    feat: branch field in new account form

--- src/main/java/Controller/NewAccountServlert.java
+++ src/main/java/Controller/NewAccountServlert.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import Connection.Conn;
 import Model.Dao;
@@ -19,6 +20,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import Classes.Branch;
+
 
 @WebServlet("/new-account")
 public class NewAccountServlert extends HttpServlet {
@@ -26,8 +29,18 @@ public class NewAccountServlert extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession session = req.getSession();
 		if(session.getAttribute("user_id") != null) {
+			try {				
+				List<Branch> branchList = Dao.getBranchList("branch_id, branch_name");
+				req.setAttribute("branch_list", branchList);
+			}
+			catch(ClassNotFoundException e){
+				e.printStackTrace();
+			}
+			catch(SQLException e){
+				e.printStackTrace();
+			}
 			RequestDispatcher rd = req.getRequestDispatcher("NewAccount.jsp");
-			rd.forward(req, resp);	
+			rd.forward(req, resp);
 		}
 		else {
 			resp.sendRedirect("login");
@@ -38,6 +51,7 @@ public class NewAccountServlert extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String newAccountId = GenerateID.randomUUID(12);
 		String customerId = (String)req.getParameter("customer_id");
+		String branch = (String)req.getParameter("branch");
 		String account_type = (String)req.getParameter("account_type");		
 		System.out.println(account_type + " " + newAccountId + "  " + customerId);
 		try {
@@ -48,12 +62,13 @@ public class NewAccountServlert extends HttpServlet {
 				email = rs.getString("customer_email");
 				phone = rs.getLong("customer_phone");
 			}
-			PreparedStatement addNewAccount = Conn.getConnectionObj().prepareStatement("insert into account(account_type, account_no, balance, customer_id, status) values(?,?,?,?,?)");
+			PreparedStatement addNewAccount = Conn.getConnectionObj().prepareStatement("insert into account(account_type, account_no, balance, customer_id, status, branch_id) values(?,?,?,?,?,?)");
 			addNewAccount.setString(1, account_type);
             addNewAccount.setString(2, newAccountId);
             addNewAccount.setInt(3, 0);
             addNewAccount.setString(4,customerId);
             addNewAccount.setString(5,"PROCESSING");
+            addNewAccount.setString(6,branch);
             int insertCount = addNewAccount.executeUpdate();
             HttpSession session = req.getSession();
             if(insertCount > 0) {
--- src/main/webapp/NewAccount.jsp
+++ src/main/webapp/NewAccount.jsp
@@ -8,6 +8,14 @@
 	<label for="customer_id"><span>Customer ID</span>
 		<input readonly="readonly" value="<%= (String)session.getAttribute("user_id") %>" name="customer_id" required="true"/>
 	</label>
+	<label><span>Branch</span>
+	<select name="branch">
+		<c:forEach items="${branch_list}" var="branch">
+			<option value="${branch.id}">${branch.name} - ${branch.id }</option>
+		</c:forEach>
+	</select>
+	</label>
+	
 <%@ include file="NewAccount.html" %>
 	<button type="submit">Submit</button>
 </form>
